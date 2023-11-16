bae98d0d861156a6f63ea7616f043d33f2ab2354
David Nickerson <david.nickerson@gmail.com>
2023-11-16 22:51:24 +1300

    initial play with some of the pyscript examples

---
+++ main.py
@@ -0,0 +1,45 @@
+from pyscript import document, display
+
+import arrr
+import matplotlib.pyplot as plt
+import matplotlib.tri as tri
+import numpy as np
+
+
+def make_plot(event):
+    # First create the x and y coordinates of the points.
+    n_angles = 36
+    n_radii = 8
+    min_radius = 0.25
+    radii = np.linspace(min_radius, 0.95, n_radii)
+
+    angles = np.linspace(0, 2 * np.pi, n_angles, endpoint=False)
+    angles = np.repeat(angles[..., np.newaxis], n_radii, axis=1)
+    angles[:, 1::2] += np.pi / n_angles
+
+    x = (radii * np.cos(angles)).flatten()
+    y = (radii * np.sin(angles)).flatten()
+    z = (np.cos(radii) * np.cos(3 * angles)).flatten()
+
+    # Create the Triangulation; no triangles so Delaunay triangulation created.
+    triang = tri.Triangulation(x, y)
+
+    # Mask off unwanted triangles.
+    triang.set_mask(np.hypot(x[triang.triangles].mean(axis=1),
+                             y[triang.triangles].mean(axis=1))
+                    < min_radius)
+
+    fig1, ax1 = plt.subplots()
+    ax1.set_aspect('equal')
+    tpc = ax1.tripcolor(triang, z, shading='flat')
+    fig1.colorbar(tpc)
+    ax1.set_title('tripcolor of Delaunay triangulation, flat shading')
+
+    display(fig1, target="output")
+
+
+def translate_english(event):
+    input_text = document.querySelector("#english")
+    english = input_text.value
+    output_div = document.querySelector("#output")
+    output_div.innerText = arrr.translate(english)
